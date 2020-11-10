fb03c9fe1d0740d7c887ef331dc6cf433014fd66
Kreetika07 <71652853+Kreetika07@users.noreply.github.com>
2020-11-10 15:58:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var  database;
+var position;
+var form, player, game;
+var playerCount, gameState=0;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  
+}
